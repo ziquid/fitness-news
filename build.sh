0851b768088297bd1f28f88273c83ef9a0ce3555
Joseph Cheek <joseph@cheek.com.co>
2015-06-26 14:39:46 -0400

    new build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ DRUSH=`which d7`
 [ "$DRUSH" == "" ] && DRUSH=`which drush`
 
 SCOPE=local
+[ "$PWD" == "/var/www/dev.fitnessne.ws/htdocs" ] && SCOPE=dev
+[ "$PWD" == "/var/www/fitnessne.ws/htdocs" ] && SCOPE=prod
 
 $DRUSH -y -q make gdg.make
 $DRUSH -y en master features
