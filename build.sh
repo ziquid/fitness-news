ed41b4531d2dfe76da3f3002ba7d6cb76276cd4f
Joseph Cheek <joseph@gdgwebworks.com>
2016-04-18 14:30:49 -0400

    minor build process improvements

--- build.sh
+++ build.sh
@@ -5,12 +5,16 @@ DRUSH=`which d7`
 [ "$DRUSH" == "" ] && DRUSH=`which drush`
 
 SCOPE=local
-[ "$PWD" == "/var/www/dev.fitnessne.ws/htdocs" ] && SCOPE=dev
-[ "$PWD" == "/var/www/fitnessne.ws/htdocs" ] && SCOPE=prod
+echo "$PWD" | grep -s -q htdocs && SCOPE=prod
+echo "$PWD" | grep -s -q dev && SCOPE=dev
 
+chmod ug+w sites/default
+cp .gitignore gitignore
 $DRUSH -y -q make gdg.make
+mv gitignore .gitignore
 $DRUSH -y en master features
+echo enabling modules with a scope of $SCOPE
 $DRUSH -y master-execute --scope=$SCOPE
 $DRUSH -y fra
 $DRUSH -y updb
-$DRUSH cc all
+# $DRUSH cc all
